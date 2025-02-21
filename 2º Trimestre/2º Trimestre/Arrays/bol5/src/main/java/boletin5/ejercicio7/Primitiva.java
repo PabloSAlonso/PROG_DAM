277b36b811bdf73e720ddc807c678358c5192c09
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-02-21 13:04:23 +0100

    changes

--- 2º Trimestre/2º Trimestre/Arrays/bol5/src/main/java/boletin5/ejercicio7/Primitiva.java
+++ 2º Trimestre/2º Trimestre/Arrays/bol5/src/main/java/boletin5/ejercicio7/Primitiva.java
@@ -1,58 +1,65 @@
 package boletin5.ejercicio7;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Primitiva {
     /**
-     * Funcion que contiene una colección que vacía al inicio y la rellena con 6 valores aleatorios entre 1 y 49
+     * Funcion que contiene una colección que vacía al inicio y la rellena con 6
+     * valores aleatorios entre 1 y 49
+     * 
      * @param coleccion la colección vacía que rellena
      * @return la colección rellenada
      */
-    public static void rellenaCol(ArrayList<Integer> coleccion){
-        coleccion.clear(); 
+    public static void rellenaCol(ArrayList<Integer> coleccion) {
+        coleccion.clear();
         int numero;
-        for (int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++) {
             numero = (int) (Math.random() * 49 + 1);
-            if (coleccion.contains(numero)){
+            if (coleccion.contains(numero)) {
                 i--;
             } else {
                 coleccion.add(numero);
             }
         }
     }
+
     /**
-     * Funcion que compara los numeros de dos colecciones y va sumando en un acumulador el numero de coincidencias
+     * Funcion que compara los numeros de dos colecciones y va sumando en un
+     * acumulador el numero de coincidencias
+     * 
      * @param numsUsuario La coleccion de numeros introducida por el usuario
      * @param numsLoteria La coleccion de numeros de la loteria
      * @return el recuento de coincidencias
      */
-    public static int compara(ArrayList<Integer> numsUsuario, ArrayList<Integer> numsLoteria){
+    public static int compara(ArrayList<Integer> numsUsuario, ArrayList<Integer> numsLoteria) {
         int acumulador = 0;
-        for (int i = 0; i < numsUsuario.size(); i++){
-            for(int j = 0; j < numsLoteria.size(); j++){
-                if (numsUsuario.get(i) == numsLoteria.get(j)){
-                    acumulador ++;
+        for (int i = 0; i < numsUsuario.size(); i++) {
+            for (int j = 0; j < numsLoteria.size(); j++) {
+                if (numsUsuario.get(i) == numsLoteria.get(j)) {
+                    acumulador++;
                 }
             }
         }
         return acumulador;
     }
+
     /**
      * 
      * @return
      */
-    public static ArrayList<Integer> pedirNums(){
+    public static ArrayList<Integer> pedirNums() {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> numeroUsuario = new ArrayList<>();
         String lista;
         boolean comprobado = false;
-        while (!comprobado){
+        while (!comprobado) {
             numeroUsuario = new ArrayList<>();
             System.out.println("Introduce seis numeros separados por coma: ");
             lista = sc.nextLine();
             String[] listaSeparacion = lista.split(",");
 
-            for (String numero : listaSeparacion){
+            for (String numero : listaSeparacion) {
                 numeroUsuario.add(Integer.parseInt(numero));
             }
             comprobado = validarNums(numeroUsuario);
@@ -62,12 +69,15 @@ public class Primitiva {
         }
         return numeroUsuario;
     }
+
     /**
-     * Función que comprueba si un numero está entre 1 y 49, en caso de no estarlo lo pide hasta que lo este
+     * Función que comprueba si un numero está entre 1 y 49, en caso de no estarlo
+     * lo pide hasta que lo este
+     * 
      * @param n el numero que se comprueba
      * @return true si es valido en el rango, false si no lo es
      */
-    public static boolean comprobarNum(int n){
+    public static boolean comprobarNum(int n) {
         Scanner sc = new Scanner(System.in);
         boolean flag = true;
         while (n < 1 || n > 49) {
@@ -79,42 +89,47 @@ public class Primitiva {
         sc.close();
         return flag;
     }
+
     /**
      * 
      * @param numeros
      * @return
      */
-    public static boolean validarNums(ArrayList<Integer> numeros){
+    public static boolean validarNums(ArrayList<Integer> numeros) {
         int num;
-        if (numeros.size() != 6){
+        if (numeros.size() != 6) {
             return false;
         }
-        for (int i = 0; i < numeros.size(); i++){
+        for (int i = 0; i < numeros.size(); i++) {
             num = numeros.get(i);
-            if (comprobarNum(num) == false){
+            if (comprobarNum(num) == false) {
                 return false;
             }
-            for (int j = i + 1; j < numeros.size(); j++){
-                if (num == numeros.get(j)){
+            for (int j = i + 1; j < numeros.size(); j++) {
+                if (num == numeros.get(j)) {
                     return false;
                 }
             }
         }
         return true;
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> numsUsuario = new ArrayList<>();
         ArrayList<Integer> numsLoteria = new ArrayList<>();
-        int [] numAciertos = new int[7];
-        
+        int[] numAciertos = new int[7];
+        numsUsuario = pedirNums();
         int resultado;
-        for (int i  = 0; i < 1000000; i++){
-            
+        for (int i = 0; i < 1000000; i++) {
+            rellenaCol(numsLoteria);
+            resultado = compara(numsUsuario, numsLoteria);
+            numAciertos[resultado]++;
         }
-
-
+        System.out.println("Resultados: ");
+        for (int i = 0; i < numAciertos.length; i++) {
+            System.out.printf("%d aciertos: %2d\n", i, numAciertos[i]);
+        }
         sc.close();
     }
 }
