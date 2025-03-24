7374e4ed37f52a4839748d2c70492e906e7e6582
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-03-24 16:59:45 +0100

    changes

--- 3º Trimestre/excepciones/src/main/java/exception/ejercicio3/ejercicio3.java
+++ 3º Trimestre/excepciones/src/main/java/exception/ejercicio3/ejercicio3.java
@@ -1,35 +1,58 @@
 package exception.ejercicio3;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class ejercicio3 {
-    public static void pedirRobusto(String mensaje, int minimo, int maximo){
+    public static void pedirRobusto(String mensaje, int minimo, int maximo) {
         Scanner sc = new Scanner(System.in);
         System.out.println(mensaje);
-        int cantidad;
-        if (mensaje == "" || mensaje == null || maximo - minimo < 2 || minimo > maximo){
+        int cantidad = 0;
+        if (maximo - minimo < 2 || minimo > maximo) {
             throw new IllegalArgumentException("Parametros incorrectos");
+        }
+        if (mensaje == "" || mensaje == null) {
+            throw new IllegalArgumentException("Mensaje incorrecto");
         } else {
             System.out.println("Introduce un numero entero");
-            cantidad = sc.nextInt();
-            while (cantidad < minimo || cantidad > maximo) {
-                System.out.println("Introduce un valor dentro del rango");
+            try {
                 cantidad = sc.nextInt();
+                while (cantidad < minimo || cantidad > maximo) {
+                    System.out.println("Introduce un valor dentro del rango");
+                    cantidad = sc.nextInt();
+                }
+            } catch (InputMismatchException m) {
+                System.out.println("Introduce un caracter valido");
+                sc.nextLine();
+                cantidad = minimo - 1;
             }
         }
         sc.close();
     }
+
     public static void main(String[] args) {
-        try{
+        try {
+            System.out.println("Primer caso");
             pedirRobusto("Holaaa", 2, 5);
-            pedirRobusto(null, 1, 5);
-        } catch (IllegalArgumentException i)  {
+        } catch (IllegalArgumentException i) {
             System.out.println("Parametro no válido");
-        } try {
+        }
+        try {
+            System.out.println("Segundo caso");
             pedirRobusto("Hola", 1, 2);
-        } catch (IllegalArgumentException i)  {
+        } catch (IllegalArgumentException i) {
             System.out.println("Parametro no válido");
-        } try {
+        }
+        try {
+            System.out.println("Tercer caso");
             pedirRobusto("Holaa", 5, 2);
-        } catch (IllegalArgumentException i)  {
+        } catch (IllegalArgumentException i) {
+            System.out.println("Parametro no válido");
+        }
+        try {
+            System.out.println("Cuarto caso");
+            pedirRobusto(null, 1, 5);
+        } catch (IllegalArgumentException i) {
             System.out.println("Parametro no válido");
         }
     }
