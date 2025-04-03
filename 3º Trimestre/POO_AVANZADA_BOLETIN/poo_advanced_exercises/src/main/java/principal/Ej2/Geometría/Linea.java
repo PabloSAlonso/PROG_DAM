2d3f63b873385ff3353b30c4f5a4d3c166db6ab5
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-04-03 13:54:18 +0200

    changes

--- 3º Trimestre/POO_AVANZADA_BOLETIN/poo_advanced_exercises/src/main/java/principal/Ej2/Geometría/Linea.java
+++ 3º Trimestre/POO_AVANZADA_BOLETIN/poo_advanced_exercises/src/main/java/principal/Ej2/Geometría/Linea.java
@@ -1,5 +1,36 @@
 package principal.Ej2.Geometría;
 
-public class Linea {
-    
+import principal.Ej2.Interfaz.Libreria;
+
+public class Linea extends Figura {
+    private Punto puntoFinal;
+
+    public void setPuntoFinal(Punto puntoFinal) {
+        this.puntoFinal = puntoFinal;
+    }
+
+    public Punto getPuntoFinal() {
+        return puntoFinal;
+    }
+
+    @Override
+    public void pedirDatos() {
+        super.pedirDatos();
+        puntoFinal = new Punto(Libreria.pedirReal(), Libreria.pedirReal());
+    }
+
+    @Override
+    public void muestraDatos() {
+        super.muestraDatos();
+        System.out.println(puntoFinal);
+    }
+
+    public Linea(Punto origen, Punto puntoFinal) {
+        super(origen, "linea");
+        setPuntoFinal(puntoFinal);
+    }
+
+    public Linea() {
+        this(new Punto(0, 0), new Punto(1, 1));
+    }
 }
