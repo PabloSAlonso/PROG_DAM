617daaa8b44ac7afb352a306c2b2014eba7fe807
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-04-07 09:47:52 +0200

    changes

---
+++ 3º Trimestre/GuiaProgGráfica/guiasgraficos/src/main/java/swing1/Application.java
@@ -0,0 +1,16 @@
+package swing1;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class Application {
+  public static void main(String[] args) {
+    
+    JOptionPane.showMessageDialog(null,
+        "Welcome to the Java World",
+        "Usando Swing",
+        JOptionPane.INFORMATION_MESSAGE);
+
+  }
+}
