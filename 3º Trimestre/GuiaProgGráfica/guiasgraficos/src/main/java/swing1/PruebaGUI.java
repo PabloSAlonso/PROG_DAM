db7bebbd15e7ba06dd8bdc4f66a3d1891dbdefb9
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-04-10 13:18:18 +0200

    changes

--- 3º Trimestre/GuiaProgGráfica/guiasgraficos/src/main/java/swing1/PruebaGUI.java
+++ 3º Trimestre/GuiaProgGráfica/guiasgraficos/src/main/java/swing1/PruebaGUI.java
@@ -3,14 +3,16 @@ import javax.swing.*;
 
 public class PruebaGUI {
     public static void main(String[] args) {
-        FrmPrincipal frame = new FrmPrincipal();
+        // FrmPrincipal frame = new FrmPrincipal();
+        Eventos1 frame = new Eventos1();
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         
-        FrmPrincipal frame2 = new FrmPrincipal();
-        frame2.setSize(300, 300);
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame2.setVisible(true);
+        // FrmPrincipal frame2 = new FrmPrincipal();
+        // frame2.setSize(300, 300);
+        // frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame2.setVisible(true);
     }
 }
---
+++ 3º Trimestre/GuiaProgGráfica/guiasgraficos/target/classes/swing1/ordenProyectosSwing.md
@@ -0,0 +1,11 @@
+# Normas Orden Swing
+
+* Mínimo dos clases  
+
+* Declaraciones de variables (etiquetas, botones...)  
+
+* Super y setLayout  
+
+* Comentario antes de inicializar cada elemtento  
+
+*  Instanciamos cada elementos y lo configuramos, formando una especie de bloques de código, cada uno bien identificado y fácilmente visible.  
