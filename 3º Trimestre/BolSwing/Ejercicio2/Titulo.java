cbb2285cc5695f561d08127441d47a52f09f42ae
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-04-28 12:24:51 +0200

    changes

--- 3º Trimestre/BolSwing/Ejercicio2/Titulo.java
+++ 3º Trimestre/BolSwing/Ejercicio2/Titulo.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -10,6 +11,8 @@ public class Titulo extends JFrame implements ActionListener {
     private JTextField texto;
     private JButton boton;
     private JOptionPane pregunta;
+    private JCheckBox alReves;
+    private JCheckBox habilitar;
 
     public Titulo() {
         this.setTitle("Titulooo");
@@ -23,6 +26,12 @@ public class Titulo extends JFrame implements ActionListener {
         boton.addActionListener(this);
 
         pregunta = new JOptionPane();
+
+        alReves = new JCheckBox("Dar la vuelta");
+        this.add(alReves);
+
+        habilitar = new JCheckBox("Deshabilitar/habilitar textField");
+        this.add(habilitar); //TODO acabar segundo textField
     }
 
     @Override
@@ -31,5 +40,12 @@ public class Titulo extends JFrame implements ActionListener {
             this.setTitle(texto.getText());
         }
         
+        if (alReves.isSelected()){
+            String fraseReversa = "";
+            for (int i = texto.getText().length() - 1; i >= 0; i--) {
+                fraseReversa += texto.getText().charAt(i);
+            }
+            this.setTitle(fraseReversa);
+        }
     }
 }
